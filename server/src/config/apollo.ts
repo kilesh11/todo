ec0720d2b2a914357d44eaaa205a4842d51e8c54
kyle <kylesuen@setsailsoftware.com>
2021-11-02 13:51:35 +0800

    upgrade server

--- server/src/config/apollo.ts
+++ server/src/config/apollo.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { ApolloServer, IResolvers } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import resolvers from '../resolvers/indexResolvers';
 import typeDefs from '../typeDefs/indexTypeDefs';
 import todoLoader from '../dataLoaders/todo';
@@ -7,7 +7,7 @@ import { checkAuth } from '../middleware/fireabaseMiddleware';
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers: resolvers as IResolvers,
+    resolvers,
     context: async ({ req, res }) =>
         await checkAuth(req, { req, res, dataLoaders: { todoLoader } }),
 });
